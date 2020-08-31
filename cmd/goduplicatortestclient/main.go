e02b17a321c1a4583e3ae7e317fb37bb3bdb41ac
Marko Kevac <marko@kevac.org>
2020-09-01 00:01:17 +0300

    fix up goduplicatortestclient

--- cmd/goduplicatortestclient/main.go
+++ cmd/goduplicatortestclient/main.go
@@ -3,25 +3,41 @@ package main
 import (
 	"bufio"
 	"bytes"
+	"context"
 	"flag"
+	"log"
+	"net"
+	"os"
+	"os/signal"
+	"sync"
 	"sync/atomic"
+	"syscall"
 	"time"
 )
-import "log"
-import "net"
 
 var (
 	serverAddress string
 	parallel      uint
 	requests      uint64
 	messageSize   uint
+	wg            sync.WaitGroup
 )
 
-func client() {
+func client(ctx context.Context) {
+	defer wg.Done()
+
 	c, err := net.Dial("tcp", serverAddress)
 	if err != nil {
 		log.Fatalf("error while connecting to server: %s", err)
 	}
+
+	var done uint64
+
+	go func() {
+		<-ctx.Done()
+		atomic.AddUint64(&done, 1)
+
+	}()
 
 	r := bufio.NewReader(c)
 
@@ -40,27 +56,44 @@ func client() {
 		}
 
 		atomic.AddUint64(&requests, 1)
+		if atomic.LoadUint64(&done) > 0 {
+			return
+		}
 	}
 }
 
 func main() {
-
 	flag.UintVar(&messageSize, "s", 1024, "message size in bytes")
 	flag.StringVar(&serverAddress, "a", ":11000", "server address to connect to")
 	flag.UintVar(&parallel, "p", 10, "how many parallel connections")
 	flag.Parse()
 
+	signalChan := make(chan os.Signal, 1024)
+
+	signal.Notify(signalChan, syscall.SIGINT)
+
+	ctx, cancel := context.WithCancel(context.Background())
+
 	for i := uint(0); i < parallel; i++ {
-		go client()
+		wg.Add(1)
+		go client(ctx)
 	}
 
+	oldValue := atomic.LoadUint64(&requests)
+	ticker := time.Tick(time.Second)
+
 	for {
-		oldValue := atomic.LoadUint64(&requests)
 		select {
-		case <-time.Tick(time.Second):
+		case <-ticker:
 			newValue := atomic.LoadUint64(&requests)
 			log.Printf("%v req/sec", newValue-oldValue)
 			oldValue = newValue
+		case s := <-signalChan:
+			log.Printf("received signal %v, waiting for all clients to exit", s)
+			cancel()
+			wg.Wait()
+			log.Printf("exiting...")
+			return
 		}
 	}
 }
