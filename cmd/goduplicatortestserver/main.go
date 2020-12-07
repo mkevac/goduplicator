173b577db2f34b610e02657726018c93e5787a11
Marko Kevac <marko@kevac.org>
2020-12-08 00:58:20 +0300

    added quick rps counter to server

--- cmd/goduplicatortestserver/main.go
+++ cmd/goduplicatortestserver/main.go
@@ -6,9 +6,25 @@ import (
 	"io"
 	"log"
 	"net"
+	"sync/atomic"
+	"time"
 )
 
+func statsPrinter() {
+	var rSaved uint64 = 0
+
+	for range time.Tick(time.Second) {
+		c := atomic.LoadUint64(&connections)
+		r := atomic.LoadUint64(&requests)
+
+		log.Printf("%v c, %v rps", c, r-rSaved)
+		rSaved = r
+	}
+}
+
 func handleConnection(c net.Conn) {
+	atomic.AddUint64(&connections, 1)
+	defer atomic.AddUint64(&connections, ^uint64(1))
 	defer c.Close()
 
 	r := bufio.NewReader(c)
@@ -22,14 +38,19 @@ func handleConnection(c net.Conn) {
 			return
 		}
 
+		atomic.AddUint64(&requests, 1)
+
 		_, err = c.Write(l)
 		if err != nil {
-			log.Fatalf("error while writing: %s", err)
+			log.Printf("error while writing: %s", err)
+			return
 		}
 	}
 }
 
 var listeningAddress string
+var connections uint64
+var requests uint64
 
 func main() {
 	flag.StringVar(&listeningAddress, "l", ":11000", "listening address")
@@ -39,6 +60,8 @@ func main() {
 	if err != nil {
 		log.Fatalf("error while listening: %s", err)
 	}
+
+	go statsPrinter()
 
 	for {
 		c, err := l.Accept()
